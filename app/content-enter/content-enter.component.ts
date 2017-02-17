135e01ebe8dacede93e40e4db2b346e3029b6c1b
Jared Beagley <Jared Beagley>
2017-02-17 06:21:22 -0700

    working on basic UI

--- app/content-enter/content-enter.component.ts
+++ app/content-enter/content-enter.component.ts
@@ -1,9 +1,11 @@
 import {Component, View} from "angular2/core";
+import {PaperBack} from "../paper-back/paper-back.component";
 
 @Component({
    selector: 'content-enter',
    templateUrl: './app/content-enter/content-enter.component.html',
-   styleUrls: ['./app/content-enter/content-enter.component.css']
+   styleUrls: ['./app/content-enter/content-enter.component.css'],
+   directives: [PaperBack]
 })
 
 export class ContentEnter {
---
+++ app/paper-back/paper-back.component.ts
@@ -0,0 +1,12 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'paper-back',
+   templateUrl: './app/paper-back/paper-back.component.html',
+   styleUrls: ['./app/paper-back/paper-back.component.css']
+})
+
+export class PaperBack {
+  constructor() { }  
+
+}
