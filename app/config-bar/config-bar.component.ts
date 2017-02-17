f2c6658b297c257f400e37a78fcf10e363737478
Jared Beagley <Jared Beagley>
2017-02-16 20:47:33 -0700

    fixing errors

---
+++ app/config-bar/config-bar.component.ts
@@ -0,0 +1,12 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'config-bar',
+   templateUrl: './app/config-bar/config-bar.component.html',
+   styleUrls: ['./app/config-bar/config-bar.component.css']
+})
+
+export class ConfigBar {
+  constructor() { }  
+
+}
---
+++ app/template/config-bar.component.ts
@@ -0,0 +1,11 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'template',
+   templateUrl: './app/config-bar/config-bar.component.html'
+})
+
+export class AppComponent {
+  constructor() { }  
+
+}
